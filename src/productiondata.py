77a0a3cf16617dc96c8000b52a446106dd449869
mfirat <muratfirat78@gmail.com>
2026-09-21 16:59:16 +0200

    update

--- src/productiondata.py
+++ src/productiondata.py
@@ -87,6 +87,7 @@ class ProductionDataManager(DataManager):
             if not self.getOperationsManager().getSimulator().getController().isOnline(): 
                 Resources_df = pd.read_csv(abs_file_path+'/'+filename)
             else:
+                print("filename: "+filename)
                 Resources_df = pd.read_csv(source_directory+'/'+filename)
            
 
