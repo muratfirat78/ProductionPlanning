6b1a7acd8ca73b054ed183b787db009f85f1f0a7
mfirat <muratfirat78@gmail.com>
2026-09-21 11:04:30 +0200

    Update remotemanager.py

--- src/remotemanager.py
+++ src/remotemanager.py
@@ -13,31 +13,19 @@ import httplib2
 from google.auth import default as get_default_credentials
 from google_auth_httplib2 import AuthorizedHttp
 
+gitfolder = 'https://raw.githubusercontent.com/muratfirat78/ProductionPlanning/refs/heads/main/'
+module_files =  ['stochastic.py','simulationobjects.py','simulator.py','visual.py','controller.py'
+                , 'productionobjects.py','productionalgs.py','productiondata.py','productionmain.py','MILPScheduling.py'
+                , 'logisticsobjects.py','logisticsalgs.py','logisticsmain.py','logisticsdata.py']
 
-gitfolder = 'https://raw.githubusercontent.com/muratfirat78/ProductionPlanning/refs/heads/main/usecases/'
+usecasefolder = gitfolder+'usecases/'
 source_directory ='/content/'
-
-!wget https://raw.githubusercontent.com/muratfirat78/ProductionPlanning/refs/heads/main/src/stochastic.py
-!wget https://raw.githubusercontent.com/muratfirat78/ProductionPlanning/refs/heads/main/src/simulationobjects.py
-!wget https://raw.githubusercontent.com/muratfirat78/ProductionPlanning/refs/heads/main/src/productionobjects.py
-!wget https://raw.githubusercontent.com/muratfirat78/ProductionPlanning/refs/heads/main/src/productionalgs.py
-!wget https://raw.githubusercontent.com/muratfirat78/ProductionPlanning/refs/heads/main/src/productiondata.py
-!wget https://raw.githubusercontent.com/muratfirat78/ProductionPlanning/refs/heads/main/src/MILPScheduling.py
-!wget https://raw.githubusercontent.com/muratfirat78/ProductionPlanning/refs/heads/main/src/logisticsobjects.py
-!wget https://raw.githubusercontent.com/muratfirat78/ProductionPlanning/refs/heads/main/src/logisticsalgs.py
-!wget https://raw.githubusercontent.com/muratfirat78/ProductionPlanning/refs/heads/main/src/logisticsdata.py
-!wget https://raw.githubusercontent.com/muratfirat78/ProductionPlanning/refs/heads/main/src/productionmain.py
-!wget https://raw.githubusercontent.com/muratfirat78/ProductionPlanning/refs/heads/main/src/logisticsmain.py
-!wget https://raw.githubusercontent.com/muratfirat78/ProductionPlanning/refs/heads/main/src/simulator.py
-!wget https://raw.githubusercontent.com/muratfirat78/ProductionPlanning/refs/heads/main/src/visual.py
-!wget https://raw.githubusercontent.com/muratfirat78/ProductionPlanning/refs/heads/main/src/controller.py
-
-
 input_files =  ['Production_Decisions.csv','Production_EventTypes.csv','Production_PrecedenceInfo.csv']
 
-for filename in input_files:
-    if not filename in os.listdir(source_directory):
-      !wget "{gitfolder}{filename}"
+
+
+    return
+    
 
 
 
